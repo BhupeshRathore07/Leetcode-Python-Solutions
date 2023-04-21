48cbd4953cb10d541276d35c39a8a1e165d0c9d6
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-04-21 10:48:38 +0530

    Create 0003 - Longest Substring Without Repeating Characters.py

---
+++ 0003 - Longest Substring Without Repeating Characters.py
@@ -0,0 +1,57 @@
+# Given a string s, find the length of the longest 
+# substring
+#  without repeating characters.
+
+ 
+
+# Example 1:
+
+# Input: s = "abcabcbb"
+# Output: 3
+# Explanation: The answer is "abc", with the length of 3.
+# Example 2:
+
+# Input: s = "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: s = "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3.
+# Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ 
+
+# Constraints:
+
+# 0 <= s.length <= 5 * 104
+# s consists of English letters, digits, symbols and spaces.
+
+
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s== "":
+            return 0
+        
+        start = 0
+        end = 0
+        maxLength = 0
+        uniqueCharacters = set()
+        
+        while end < len(s):
+            
+            if s[end] not in uniqueCharacters:
+                uniqueCharacters.add(s[end])
+                end += 1
+                maxLength = max(maxLength, len(uniqueCharacters))
+            
+            else:
+                uniqueCharacters.remove(s[start])
+                start += 1
+        
+        return maxLength
