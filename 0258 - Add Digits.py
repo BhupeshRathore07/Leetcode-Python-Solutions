0950f2a5cb54fdcb309c9fcb04b1e9040f474c5a
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-04-26 11:10:09 +0530

    Create 0258 - Add Digits.py

---
+++ 0258 - Add Digits.py
@@ -0,0 +1,56 @@
+# Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+
+# Example 1:
+
+# Input: num = 38
+# Output: 2
+# Explanation: The process is
+# 38 --> 3 + 8 --> 11
+# 11 --> 1 + 1 --> 2 
+# Since 2 has only one digit, return it.
+# Example 2:
+
+# Input: num = 0
+# Output: 0
+ 
+
+# Constraints:
+
+# 0 <= num <= 231 - 1
+ 
+
+# Follow up: Could you do it without any loop/recursion in O(1) runtime?
+
+
+
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        while len(str(num)) > 1:
+            res = 0
+
+            for i in str(num):
+                res += int(i)
+            num = res
+        
+        return num
+      
+      
+# ALTERNATIVE IN O(1):
+
+
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        return num % 9
