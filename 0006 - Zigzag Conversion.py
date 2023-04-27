881099093fbaa1dcbcf4c39d23f7e2489edf2a37
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-04-27 05:57:37 +0530

    Create 0006 - Zigzag Conversion.py

---
+++ 0006 - Zigzag Conversion.py
@@ -0,0 +1,98 @@
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# And then read line by line: "PAHNAPLSIIGYIR"
+
+# Write the code that will take a string and make this conversion given a number of rows:
+
+# string convert(string s, int numRows);
+ 
+
+# Example 1:
+
+# Input: s = "PAYPALISHIRING", numRows = 3
+# Output: "PAHNAPLSIIGYIR"
+# Example 2:
+
+# Input: s = "PAYPALISHIRING", numRows = 4
+# Output: "PINALSIGYAHRPI"
+# Explanation:
+# P     I    N
+# A   L S  I G
+# Y A   H R
+# P     I
+# Example 3:
+
+# Input: s = "A", numRows = 1
+# Output: "A"
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 1000
+# s consists of English letters (lower-case and upper-case), ',' and '.'.
+# 1 <= numRows <= 1000
+# The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+# P   A   H   N
+# A P L S I I G
+# Y   I   R
+# And then read line by line: "PAHNAPLSIIGYIR"
+
+# Write the code that will take a string and make this conversion given a number of rows:
+
+# string convert(string s, int numRows);
+ 
+
+# Example 1:
+
+# Input: s = "PAYPALISHIRING", numRows = 3
+# Output: "PAHNAPLSIIGYIR"
+# Example 2:
+
+# Input: s = "PAYPALISHIRING", numRows = 4
+# Output: "PINALSIGYAHRPI"
+# Explanation:
+# P     I    N
+# A   L S  I G
+# Y A   H R
+# P     I
+# Example 3:
+
+# Input: s = "A", numRows = 1
+# Output: "A"
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 1000
+# s consists of English letters (lower-case and upper-case), ',' and '.'.
+# 1 <= numRows <= 1000
+
+
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1:
+            return s
+        
+        ans  = ""
+        
+        for row in range(numRows):
+            inc = 2 * (numRows - 1)
+            
+            for i in range(row, len(s), inc):
+                ans += s[i]
+                
+                if ((row > 0) and (row < numRows - 1) and (i + inc - 2 * row < len(s))):
+                    ans += s[i + inc - 2 * row]
+        
+        return ans
+      
+      
