05ed4e3a07aec0a554bab2095b8181b477399d04
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-05-23 18:27:14 +0530

    Create 0703 - Kth Largest Element in a Stream.py

---
+++ 0703 - Kth Largest Element in a Stream.py
@@ -0,0 +1,67 @@
+# Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+# Implement KthLargest class:
+
+# KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+# int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+ 
+
+# Example 1:
+
+# Input
+# ["KthLargest", "add", "add", "add", "add", "add"]
+# [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+# Output
+# [null, 4, 5, 5, 8, 8]
+
+# Explanation
+# KthLargest kthLargest = new KthLargest(3, [4, 5, 8, 2]);
+# kthLargest.add(3);   // return 4
+# kthLargest.add(5);   // return 5
+# kthLargest.add(10);  // return 5
+# kthLargest.add(9);   // return 8
+# kthLargest.add(4);   // return 8
+ 
+
+# Constraints:
+
+# 1 <= k <= 104
+# 0 <= nums.length <= 104
+# -104 <= nums[i] <= 104
+# -104 <= val <= 104
+# At most 104 calls will be made to add.
+# It is guaranteed that there will be at least k elements in the array when you search for the kth element.
+
+
+
+
+
+
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.h = nums
+        heapq.heapify(self.h)
+        while len(self.h) > k:
+            heapq.heappop(self.h)
+        
+
+    def add(self, val: int) -> int:
+        if len(self.h) < self.k:
+            heapq.heappush(self.h, val)
+        else:
+            if val > self.h[0]:
+                heapq.heappushpop(self.h, val)
+        return self.h[0]
+        
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+
+
+
