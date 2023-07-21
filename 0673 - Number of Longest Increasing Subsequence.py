e1f4f0568b3bc896354d12e659c0f45a0abdad09
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-07-21 16:49:23 +0530

    Create 0673 - Number of Longest Increasing Subsequence.py

---
+++ 0673 - Number of Longest Increasing Subsequence.py
@@ -0,0 +1,47 @@
+# Given an integer array nums, return the number of longest increasing subsequences.
+
+# Notice that the sequence has to be strictly increasing.
+
+# Example 1:
+
+# Input: nums = [1,3,5,4,7]
+# Output: 2
+# Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+# Example 2:
+
+# Input: nums = [2,2,2,2,2]
+# Output: 5
+# Explanation: The length of the longest increasing subsequence is 1, and there are 5 increasing subsequences of length 1, so output 5.
+ 
+# Constraints:
+
+# 1 <= nums.length <= 2000
+# -106 <= nums[i] <= 106
+
+
+
+
+
+
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 1:
+            return n
+
+        lengths = [1] * n
+        counts = [1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    if lengths[j] + 1 > lengths[i]:
+                        lengths[i] = lengths[j] + 1
+                        counts[i] = counts[j]
+                    elif lengths[j] + 1 == lengths[i]:
+                        counts[i] += counts[j]
+
+        max_length = max(lengths)
+        return sum(count for length, count in zip(lengths, counts) if length == max_length)
+
+
