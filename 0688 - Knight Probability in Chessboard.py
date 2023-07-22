77b4242a130c3716c5fb13b0a0a0dff65e50cfc6
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-07-22 20:54:39 +0530

    Create 0688 - Knight Probability in Chessboard.py

---
+++ 0688 - Knight Probability in Chessboard.py
@@ -0,0 +1,55 @@
+# On an n x n chessboard, a knight starts at the cell (row, column) and attempts to make exactly k moves. The rows and columns are 0-indexed, so the top-left cell is (0, 0), and the bottom-right cell is (n - 1, n - 1).
+
+# A chess knight has eight possible moves it can make, as illustrated below. Each move is two cells in a cardinal direction, then one cell in an orthogonal direction.
+
+# Each time the knight is to move, it chooses one of eight possible moves uniformly at random (even if the piece would go off the chessboard) and moves there.
+
+# The knight continues moving until it has made exactly k moves or has moved off the chessboard.
+
+# Return the probability that the knight remains on the board after it has stopped moving.
+
+# Example 1:
+
+# Input: n = 3, k = 2, row = 0, column = 0
+# Output: 0.06250
+# Explanation: There are two moves (to (1,2), (2,1)) that will keep the knight on the board.
+# From each of those positions, there are also two moves that will keep the knight on the board.
+# The total probability the knight stays on the board is 0.0625.
+# Example 2:
+
+# Input: n = 1, k = 0, row = 0, column = 0
+# Output: 1.00000
+
+# Constraints:
+
+# 1 <= n <= 25
+# 0 <= k <= 100
+# 0 <= row, column <= n - 1
+
+
+
+
+
+
+class Solution:
+    def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
+        if k == 0:
+            return 1
+
+        @cache
+        def dfs(r: int, c: int, cur_moves: int) -> int:
+            if r < 0 or r >= n or c < 0 or c >= n:
+                return 0
+            if cur_moves == k:
+                return 1
+            on_board = 0
+            for rd, cd in [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (2, -1), (2, 1), (1, -2), (1, 2)]:
+                on_board += dfs(r + rd, c + cd, cur_moves + 1)
+            return on_board
+        
+        on_board = dfs(row, column, 0)
+        return on_board / pow(8, k)
+
+
+
+
