35d65b7fbaed91b1d0e1ce0215ebfb7baaadfe3f
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-10-09 09:30:10 +0530

    Create 0034 - Find First and Last Position of Element in Sorted Array.py

---
+++ 0034 - Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,62 @@
+# Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+# If target is not found in the array, return [-1, -1].
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+# Example 1:
+
+# Input: nums = [5,7,7,8,8,10], target = 8
+# Output: [3,4]
+# Example 2:
+
+# Input: nums = [5,7,7,8,8,10], target = 6
+# Output: [-1,-1]
+# Example 3:
+
+# Input: nums = [], target = 0
+# Output: [-1,-1] 
+
+# Constraints:
+
+# 0 <= nums.length <= 105
+# -109 <= nums[i] <= 109
+# nums is a non-decreasing array.
+# -109 <= target <= 109
+
+
+
+
+
+
+
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def binary_search(nums, target, left):
+            low, high = 0, len(nums) - 1
+            index = -1
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] == target:
+                    index = mid
+                    if left:
+                        high = mid - 1
+                    else:
+                        low = mid + 1
+                elif nums[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return index
+
+        left_index = binary_search(nums, target, left=True)
+        right_index = binary_search(nums, target, left=False)
+
+        return [left_index, right_index]
+
+
+
+
+
+
