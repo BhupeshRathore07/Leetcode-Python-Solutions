e629246d0ea63722461128b0b4618c2eb9b8b4ac
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-10-11 19:33:40 +0530

    Create 2251 - Number of Flowers in Full Bloom.py

---
+++ 2251 - Number of Flowers in Full Bloom.py
@@ -0,0 +1,56 @@
+# You are given a 0-indexed 2D integer array flowers, where flowers[i] = [starti, endi] means the ith flower will be in full bloom from starti to endi (inclusive). You are also given a 0-indexed integer array people of size n, where people[i] is the time that the ith person will arrive to see the flowers.
+
+# Return an integer array answer of size n, where answer[i] is the number of flowers that are in full bloom when the ith person arrives.
+
+# Example 1:
+
+# Input: flowers = [[1,6],[3,7],[9,12],[4,13]], poeple = [2,3,7,11]
+# Output: [1,2,2,2]
+# Explanation: The figure above shows the times when the flowers are in full bloom and when the people arrive.
+# For each person, we return the number of flowers in full bloom during their arrival.
+# Example 2:
+
+# Input: flowers = [[1,10],[3,3]], poeple = [3,3,2]
+# Output: [2,2,1]
+# Explanation: The figure above shows the times when the flowers are in full bloom and when the people arrive.
+# For each person, we return the number of flowers in full bloom during their arrival.
+ 
+# Constraints:
+
+# 1 <= flowers.length <= 5 * 104
+# flowers[i].length == 2
+# 1 <= starti <= endi <= 109
+# 1 <= people.length <= 5 * 104
+# 1 <= people[i] <= 109
+
+
+
+
+
+
+
+
+class Solution:
+    def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
+        blooming = defaultdict(int)                
+        for start, end in flowers:
+            blooming[start] += 1
+            blooming[end+1] -= 1
+
+        ans, bloom_cnt = {}, 0
+        h = sorted(people,reverse=True)           
+        for time in sorted(blooming.keys()):        
+            bloom_change = blooming[time]
+            while h and time > h[-1]:               
+                ans[h.pop()] = bloom_cnt            
+            if not h: break
+            bloom_cnt += bloom_change
+
+        return [ans[p] if p in ans else 0 for p in people]
+
+
+
+
+
+
+
