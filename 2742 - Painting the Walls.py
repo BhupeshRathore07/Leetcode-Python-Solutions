36326bd67dd311e6f1bffa486ec72a164a4e2572
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-10-14 09:16:29 +0530

    Create 2742 - Painting the Walls.py

---
+++ 2742 - Painting the Walls.py
@@ -0,0 +1,50 @@
+# You are given two 0-indexed integer arrays, cost and time, of size n representing the costs and the time taken to paint n different walls respectively. There are two painters available:
+
+# A paid painter that paints the ith wall in time[i] units of time and takes cost[i] units of money.
+# A free painter that paints any wall in 1 unit of time at a cost of 0. But the free painter can only be used if the paid painter is already occupied.
+# Return the minimum amount of money required to paint the n walls.
+
+# Example 1:
+
+# Input: cost = [1,2,3,2], time = [1,2,3,2]
+# Output: 3
+# Explanation: The walls at index 0 and 1 will be painted by the paid painter, and it will take 3 units of time; meanwhile, the free painter will paint the walls at index 2 and 3, free of cost in 2 units of time. Thus, the total cost is 1 + 2 = 3.
+# Example 2:
+
+# Input: cost = [2,3,4,2], time = [1,1,1,1]
+# Output: 4
+# Explanation: The walls at index 0 and 3 will be painted by the paid painter, and it will take 2 units of time; meanwhile, the free painter will paint the walls at index 1 and 2, free of cost in 2 units of time. Thus, the total cost is 2 + 2 = 4.
+ 
+# Constraints:
+
+# 1 <= cost.length <= 500
+# cost.length == time.length
+# 1 <= cost[i] <= 106
+# 1 <= time[i] <= 500
+
+
+
+
+
+
+
+
+
+class Solution:
+    def paintWalls(self, cost: List[int], time: List[int]) -> int:
+        k_max = 500000000 
+        n = len(cost) 
+        dp = [k_max] * (n + 1)
+        dp[0] = 0 
+        for i in range(n):
+            for walls in range(n, 0, -1):
+                dp[walls] = min(dp[walls], dp[max(walls - time[i] - 1, 0)] + cost[i])
+        return dp[n] 
+
+
+
+
+
+
+
+
