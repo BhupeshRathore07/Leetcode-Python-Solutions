4c7879187dad48832bcdf1d190760832d8bda1a6
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-12-03 22:34:31 +0530

    Created 1266 - Minimum Time Visiting All Points.py

--- 1266 - Minimum Time Visiting All Points.py
+++ 1266 - Minimum Time Visiting All Points.py
@@ -38,13 +38,11 @@
 
 class Solution:
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
-        res = 0
+        return sum([max(abs(points[i-1][0]-points[i][0]),abs(points[i-1][1]-points[i][1])) for i in range(1,len(points))])
 
-        for i in range(1, len(points)):
-            res += max(abs(points[i][0] - points[i - 1][0]),
-                        abs(points[i][1] - points[i - 1][1]))
 
-        return res
+
+
 
 
 
