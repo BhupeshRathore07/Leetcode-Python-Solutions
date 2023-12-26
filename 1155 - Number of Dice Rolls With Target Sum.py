589522daa4035d2c17df35cd93e640b2bbb17e5e
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2023-12-26 22:36:16 +0530

    Create 1155 - Number of Dice Rolls With Target Sum.py

---
+++ 1155 - Number of Dice Rolls With Target Sum.py
@@ -0,0 +1,58 @@
+# You have n dice, and each die has k faces numbered from 1 to k.
+
+# Given three integers n, k, and target, return the number of possible ways (out of the kn total ways) to roll the dice, so the sum of the face-up numbers equals target. Since the answer may be too large, return it modulo 109 + 7.
+
+# Example 1:
+
+# Input: n = 1, k = 6, target = 3
+# Output: 1
+# Explanation: You throw one die with 6 faces.
+# There is only one way to get a sum of 3.
+# Example 2:
+
+# Input: n = 2, k = 6, target = 7
+# Output: 6
+# Explanation: You throw two dice, each with 6 faces.
+# There are 6 ways to get a sum of 7: 1+6, 2+5, 3+4, 4+3, 5+2, 6+1.
+# Example 3:
+
+# Input: n = 30, k = 30, target = 500
+# Output: 222616187
+# Explanation: The answer must be returned modulo 109 + 7.
+ 
+# Constraints:
+
+# 1 <= n, k <= 30
+# 1 <= target <= 1000.
+
+
+
+
+
+
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        mod = 10**9 + 7
+
+        prev = [0] * (target + 1)
+        curr = [0] * (target + 1)
+
+        prev[0] = 1
+
+        for i in range(1, n + 1):
+            for j in range(1, target + 1):
+                ans = 0
+                for x in range(1, k + 1):
+                    if j - x >= 0:
+                        ans += prev[j - x] % mod
+                curr[j] = ans
+            prev = curr[:]
+        return int(prev[target] % mod)
+
+
+
+
+
+
+
+
