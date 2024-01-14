d7c34d427afe336d9e269fdea97c56cbfdc6e0fa
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2024-01-14 23:20:44 +0530

    Create 1657 - Determine if Two Strings Are Close.py

---
+++ 1657 - Determine if Two Strings Are Close.py
@@ -0,0 +1,67 @@
+# Two strings are considered close if you can attain one from the other using the following operations:
+
+# Operation 1: Swap any two existing characters.
+# For example, abcde -> aecdb
+# Operation 2: Transform every occurrence of one existing character into another existing character, and do the same with the other character.
+# For example, aacabb -> bbcbaa (all a's turn into b's, and all b's turn into a's)
+# You can use the operations on either string as many times as necessary.
+
+# Given two strings, word1 and word2, return true if word1 and word2 are close, and false otherwise.
+
+# Example 1:
+
+# Input: word1 = "abc", word2 = "bca"
+# Output: true
+# Explanation: You can attain word2 from word1 in 2 operations.
+# Apply Operation 1: "abc" -> "acb"
+# Apply Operation 1: "acb" -> "bca"
+# Example 2:
+
+# Input: word1 = "a", word2 = "aa"
+# Output: false
+# Explanation: It is impossible to attain word2 from word1, or vice versa, in any number of operations.
+# Example 3:
+
+# Input: word1 = "cabbba", word2 = "abbccc"
+# Output: true
+# Explanation: You can attain word2 from word1 in 3 operations.
+# Apply Operation 1: "cabbba" -> "caabbb"
+# Apply Operation 2: "caabbb" -> "baaccc"
+# Apply Operation 2: "baaccc" -> "abbccc"
+
+# Constraints:
+
+# 1 <= word1.length, word2.length <= 105
+# word1 and word2 contain only lowercase English letters.
+
+
+
+
+
+
+
+
+
+
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+        ch_set1, ch_set2 = set(word1), set(word2)
+        if ch_set1 == ch_set2:
+            freq1, freq2 = [], []
+            for ch in ch_set1:
+                freq1.append(word1.count(ch))
+            for ch in ch_set2:
+                freq2.append(word2.count(ch))
+            freq1.sort()
+            freq2.sort()
+            return freq1 == freq2
+        return False
+
+
+
+
+
+
