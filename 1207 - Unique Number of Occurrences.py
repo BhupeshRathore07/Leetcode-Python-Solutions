a6e20ce8f72d5ec8c3d1619c624a716b3521ec43
Bhupesh Singh Rathore <70787869+BhupeshRathore07@users.noreply.github.com>
2024-01-17 11:24:15 +0530

    Create 1207 - Unique Number of Occurrences.py

---
+++ 1207 - Unique Number of Occurrences.py
@@ -0,0 +1,55 @@
+# Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+# Example 1:
+
+# Input: arr = [1,2,2,1,1,3]
+# Output: true
+# Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+# Example 2:
+
+# Input: arr = [1,2]
+# Output: false
+# Example 3:
+
+# Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+# Output: true
+ 
+# Constraints:
+
+# 1 <= arr.length <= 1000
+# -1000 <= arr[i] <= 1000
+
+
+
+
+
+
+
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        arr.sort()
+        v = []
+
+        i = 0
+        while i < len(arr):
+            cnt = 1
+
+            # Count occurrences of the current element
+            while i + 1 < len(arr) and arr[i] == arr[i + 1]:
+                cnt += 1
+                i += 1
+
+            v.append(cnt)
+            i += 1
+
+        v.sort()
+
+        for i in range(1, len(v)):
+            if v[i] == v[i - 1]:
+                return False
+
+        return True
+
+
+
